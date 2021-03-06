7a31f882af6356656d95bc4649c30f16e77a91d7
AdinaCristian20 <cristianadina18@stud.ase.ro>
2021-03-06 13:49:47 +0200

    incarcare program

---
+++ seminar02/src/program/Program.java
@@ -0,0 +1,25 @@
+package program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import clase.Angajat;
+import clase.Utils;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Angajat> listaAngajati;
+		try {
+			listaAngajati = Utils.readAngajati("angajati.txt");
+			for(Angajat angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
