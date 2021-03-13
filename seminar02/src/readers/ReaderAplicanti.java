3b66b76d4a55d532e8b2f0e7cc5741b48b063ef8
AdinaCristian20 <cristianadina18@stud.ase.ro>
2021-03-13 14:32:39 +0200

    implementare metoda citesteAplicant

--- seminar02/src/readers/ReaderAplicanti.java
+++ seminar02/src/readers/ReaderAplicanti.java
@@ -2,6 +2,7 @@ package readers;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 import clase.Aplicant;
 
@@ -13,7 +14,24 @@ public abstract class ReaderAplicanti {
 		this.fileName = fileName;
 	}
 
+	//aici trebuia readerAplicanti
 	public abstract List<Aplicant> readAplicant() throws FileNotFoundException;
 	
+	public void citesteAplicant(Scanner input, Aplicant aplicant) {
+		String nume = input.next();
+		String prenume = input.next();
+		int varsta = input.nextInt();
+		int punctaj = input.nextInt();
+		int nr = input.nextInt();
+		String[] vect = new String[5];
+		for (int i = 0; i < nr; i++)
+			vect[i] = input.next();
+		aplicant.setNume(nume);
+		aplicant.setPrenume(prenume);
+		aplicant.setVarsta(varsta);
+		aplicant.setPunctaj(punctaj);
+		aplicant.setDenumiriProiecte(vect, nr);
+		
+	}
 	
 }
