287bb77aeb3d43bad675950581bfe138047be5b6
AdinaCristian20 <cristianadina18@stud.ase.ro>
2021-03-13 14:41:40 +0200

    metoda afiseazaAplicanti in Program

--- seminar02/src/program/Program.java
+++ seminar02/src/program/Program.java
@@ -15,15 +15,19 @@ public class Program {
 		return listaAplicanti;
 	}
 	
+	public static void afiseazaAplicanti(List<Aplicant> listaAplicanti) {
+		for(Aplicant aplicant:listaAplicanti) {
+			System.out.println(aplicant.toString());
+			aplicant.afisareSumaFinantare();
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Aplicant> listaAplicanti;
 		try {
 			listaAplicanti=citesteAplicanti(new ReaderAngajati("angajati.txt"));
-			for(Aplicant aplicant:listaAplicanti) {
-				System.out.println(aplicant.toString());
-				aplicant.afisareSumaFinantare();
-			}
+			afiseazaAplicanti(listaAplicanti);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
